bc2bb08b9f9a934a95d40a98386e833249f995a0
Brian R. Bondy <netzen@gmail.com>
2015-07-09 22:35:19 -0400

    Serve static html file which will host react

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
+var Path = require('path');
 let Hapi = require('hapi');
 
-let server = new Hapi.Server();
+let server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public')
+      }
+    }
+  }
+});
 server.connection({ port: 3000 });
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Test index!');
-    }
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply.file('index.html');
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Test ' + encodeURIComponent(request.params.name) + '!');
-    }
+  method: 'GET',
+  path: '/{name}',
+  handler: function (request, reply) {
+    reply('Test ' + encodeURIComponent(request.params.name) + '!');
+  }
 });
 
 server.start(function () {
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 });
