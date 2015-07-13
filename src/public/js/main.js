e2c85a3355e5106d26c15c0f980bf38aa98319ea
Brian R. Bondy <netzen@gmail.com>
2015-07-12 21:47:07 -0400

    Open icon links in a new tab

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -12,12 +12,12 @@ class TopBar extends React.Component {
       <li><a href='/#/other'>Other</a></li>
       <li><a href='/#/about'>About</a></li>
       <li><a href='/#/contact'>Contact</a></li>
-      <li className='social'><a href='https://twitter.com/brianbondy'><span className='fa fa-twitter'/></a></li>
-      <li className='social'><a href='/feeds/rss/'><span className='fa fa-rss'/></a></li>
-      <li className='social'><a href='https://github.com/bbondy'><span className='fa fa-github'/></a></li>
-      <li className='social'><a href='http://ca.linkedin.com/in/bbondy'><span className='fa fa-linkedin'/></a></li>
-      <li className='social'><a href='https://www.youtube.com/user/bbondy'><span className='fa fa-youtube-play'/></a></li>
-      <li className='social'><a href='http://stackoverflow.com/users/3153/brian-r-bondy'><span className='fa fa fa-stack-overflow'/></a></li>
+      <li className='social'><a href='https://twitter.com/brianbondy' target='_blank'><span className='fa fa-twitter'/></a></li>
+      <li className='social'><a href='/feeds/rss/' target='_blank'><span className='fa fa-rss'/></a></li>
+      <li className='social'><a href='https://github.com/bbondy' target='_blank'><span className='fa fa-github'/></a></li>
+      <li className='social'><a href='http://ca.linkedin.com/in/bbondy' target='_blank'><span className='fa fa-linkedin'/></a></li>
+      <li className='social'><a href='https://www.youtube.com/user/bbondy' target='_blank'><span className='fa fa-youtube-play'/></a></li>
+      <li className='social'><a href='http://stackoverflow.com/users/3153/brian-r-bondy' target='_blank'><span className='fa fa fa-stack-overflow'/></a></li>
     </ul>;
   }
 }
