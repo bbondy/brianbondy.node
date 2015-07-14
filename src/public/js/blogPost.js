a35844aae22412b3081b3d983c62208e0b24e625
Brian R. Bondy <netzen@gmail.com>
2015-07-13 22:22:42 -0400

    Add code to render server fetched blog post

--- src/public/js/blogPost.js
+++ src/public/js/blogPost.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import DocumentTitle from './documentTitle.js';
+import {fetchBlogPost} from './fetch.js';
 
 export default class BlogPost extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    fetchBlogPost(this.props.params.id).then((blogPost) =>
+      this.setState({blogPost}));
+  }
+
   render() {
+    if (!this.state.blogPost) {
+      return null;
+    }
+
     return <div>
       <DocumentTitle title='Blog Post'/>
       BlogPost {this.props.params.id}!
+      <div>{this.state.blogPost.title}</div>
+      <div>{this.state.blogPost.created}</div>
+      <div>{this.state.blogPost.body}</div>
     </div>;
   }
 }
