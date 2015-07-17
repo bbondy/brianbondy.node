fd8f15b59c06d226c391a06e032a557ee147d7ec
Brian R. Bondy <netzen@gmail.com>
2015-07-16 23:46:11 -0400

    Refactor out the remote makrdown viewer into its own control

--- src/public/js/about.js
+++ src/public/js/about.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import DocumentTitle from './documentTitle.js';
-import {fetchMarkdown} from './fetch.js';
-import * as Markdown from 'markdown';
+import RemoteMarkdownViewer from './remoteMarkdownViewer.js';
 
 export default class About extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      markdownText: ''
-    };
-  }
-  componentWillMount() {
-    fetchMarkdown('about.markdown').then((markdownText) =>
-      this.setState({markdownText}));
-  }
   render() {
     return <div>
       <DocumentTitle title='About'/>
-      <div dangerouslySetInnerHTML={{__html: Markdown.markdown.toHTML(this.state.markdownText)}}/>
+      <RemoteMarkdownViewer src='about.markdown'/>
     </div>;
   }
 }
---
+++ src/public/js/remoteMarkdownViewer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {fetchMarkdown} from './fetch.js';
+import * as Markdown from 'markdown';
+
+export default class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      markdownText: ''
+    };
+  }
+  componentWillMount() {
+    fetchMarkdown(this.props.src).then((markdownText) =>
+      this.setState({markdownText}));
+  }
+  render() {
+     return <div dangerouslySetInnerHTML={{__html: Markdown.markdown.toHTML(this.state.markdownText)}}/>
+  }
+}
