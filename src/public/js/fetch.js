83ebabc0838fc3962d7e55f74695377195ee6d78
Brian R. Bondy <netzen@gmail.com>
2015-07-27 23:43:50 -0400

    Add fetchHtml function

--- src/public/js/fetch.js
+++ src/public/js/fetch.js
@@ -1,8 +1,8 @@
-var getJSON = url => {
+let getByType = (type, url) => {
   return new Promise((resolve, reject) => {
     var xhr = new XMLHttpRequest();
     xhr.open('get', url, true);
-    xhr.responseType = 'json';
+    xhr.responseType = type;
     xhr.onload = () => {
       var status = xhr.status;
       resolve(xhr.response, status);
@@ -12,20 +12,9 @@ var getJSON = url => {
   });
 };
 
-var getText = url => {
-  return new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'text';
-    xhr.onload = () => {
-      var status = xhr.status;
-      resolve(xhr.response, status);
-    };
-    xhr.onerror = e => reject(e);
-    xhr.send();
-  });
-};
-
+let getJSON = getByType.bind(null, 'json');
+let getText = getByType.bind(null, 'text');
+let getHtml = getText;
 
 /**
  * Fetch a single blog post
@@ -35,10 +24,17 @@ export function fetchBlogPost(id) {
 }
 
 /**
- * Fetch a single blog post
+ * Fetch a single markdown path
  */
 export function fetchMarkdown(id) {
   return getText(`/markdown/${id}`);
+}
+
+/**
+ * Fetch a single html fragment page
+ */
+export function fetchHtml(id) {
+  return getHtml(`/html/${id}`);
 }
 
 /**
